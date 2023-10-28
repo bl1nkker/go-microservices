e81220c3fdeb0fa1f6db591412d7f627d78f1651
bl1nkker <nurovich14@gmail.com>
2023-10-28 22:48:42 +0600

    feat: added channels and graceful shutdown

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"go-microservices/handlers"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 func main(){
@@ -17,11 +20,33 @@ func main(){
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
-	// http.HandleFunc("/bye", func(http.ResponseWriter, *http.Request) {
-	// 	log.Println("Goodbye World")
-	// })
+
+	server := &http.Server{
+		Addr: ":9090",
+		Handler: sm,
+		IdleTimeout: 120 * time.Second,
+		ReadTimeout: 1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func(){
+		err := server.ListenAndServe()
+		if err != nil{
+			logger.Fatal(err)
+		}
+	}()
+
+	signalChannel := make(chan os.Signal, 5)
+	signal.Notify(signalChannel, os.Interrupt)
+
+	logger.Println("Waiting for a signal...")
+	sig := <-signalChannel
+	logger.Println("Signal Received!")
+	logger.Println("Received terminate. Graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30 * time.Second)
+	server.Shutdown(tc)
 
 	// DefaultServeMux is an HTTP request multiplexer. It matches the URL of each incoming request against a list of 
 	// registered patterns and calls the handler for the pattern that most closely matches the URL.
-	http.ListenAndServe(":9090", sm)
 }
