9c071773265e9ac4ba3ce0dc3680dcf8d9241643
bl1nkker <nurovich14@gmail.com>
2023-10-28 22:03:45 +0600

    feat: servemux + type methods

--- main.go
+++ main.go
@@ -1,34 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"go-microservices/handlers"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main(){
 	// HandleFunc takes my func, creates an HTTP handler, and adds it to the DefaultServeMux
+	logger := log.New(os.Stdout, "go-microservices_", log.LstdFlags)
 
-	http.HandleFunc("/", func(res http.ResponseWriter, req*http.Request) {
-		// res is an interface that implements the Writer interface
-		// req is the actual pointer(!) to the request object
-		log.Println("Hello World")
-		data, err := io.ReadAll(req.Body)
+	hh := handlers.NewHello(logger)
+	gh := handlers.NewGoodbye(logger)
 
-		if err != nil{
-			http.Error(res, "Oops", http.StatusBadRequest)
-			return
-		}
-		log.Printf("Data: %s\n", data)
-
-		fmt.Fprintf(res, "Hello %s", data)
-	})
-	http.HandleFunc("/bye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye World")
-	})
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+	// http.HandleFunc("/bye", func(http.ResponseWriter, *http.Request) {
+	// 	log.Println("Goodbye World")
+	// })
 
 	// DefaultServeMux is an HTTP request multiplexer. It matches the URL of each incoming request against a list of 
 	// registered patterns and calls the handler for the pattern that most closely matches the URL.
-	http.ListenAndServe(":9090", nil)
+	http.ListenAndServe(":9090", sm)
 }
